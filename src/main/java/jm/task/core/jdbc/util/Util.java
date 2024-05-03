92eed45c54ed2719b59e484473826b59407d447a
Dmitriy Plashchenkov <supergogi39@yandex.ru>
2024-05-03 23:44:34 +0300

    Hibernate realisation

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,42 +1,70 @@
 package jm.task.core.jdbc.util;
 
 import com.mysql.jdbc.Driver;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
     private static final String URL = "jdbc:mysql://localhost:3306/test_database";
-    private static Connection CONNECTION = null;
+    private static Connection connection = null;
+    private static SessionFactory factory = null;
 
     public static Connection openConnection() {
-        if (CONNECTION != null) {
-            return CONNECTION;
+        if (connection != null) {
+            return connection;
         } else {
             try {
-                CONNECTION = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
                 System.out.println("Connected to database");
             } catch (SQLException e) {
                 System.err.println("Could not connect to database");
                 e.printStackTrace();
             }
         }
-        return CONNECTION;
+        return connection;
     }
 
     public static void closeConnection() {
-        if (CONNECTION != null) {
+        if (connection != null) {
             try {
-                CONNECTION.close();
+                connection.close();
                 System.out.println("Connection closed");
             } catch (SQLException e) {
                 System.err.println("Could not close connection");
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        Properties settings = new Properties();
+        settings.put(Environment.URL, URL);
+        settings.put(Environment.USER, USERNAME);
+        settings.put(Environment.PASS, PASSWORD);
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        settings.put(Environment.SHOW_SQL, "true");
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        settings.put(Environment.HBM2DDL_AUTO, "");
+
+        if (factory == null) {
+            factory = new Configuration().setProperties(settings)
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+        }
+        return factory;
+    }
+
+    public static void closeSessionFactory() {
+        factory.close();
     }
 }
 
